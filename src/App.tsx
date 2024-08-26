d12c03c2a5992886564dcabc9cc3f884d71940e0
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-26 09:24:24 +0200

    ✨ LoginForm : App > add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/pages/loginForm/LoginForm";
+import ErrorPage from "./components/pages/error/ErrorPage";
+import Pokedex from "./components/pages/pokedex/Pokedex";
 
 export default function App() {
   return (
-    <div>
-      <LoginForm/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path={"*"} element={<ErrorPage />} />
+      <Route path={"/"} element={<LoginForm />} />
+      <Route path={"/pokedex/:username"} element={<Pokedex />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-

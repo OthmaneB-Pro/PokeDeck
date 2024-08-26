76f5c79f67d292dccf0e0c81457e1bbc5bdbec1c
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-26 09:25:06 +0200

    ✨ LoginForm > Pokedex : give the username in pokedex with useParams

--- src/components/pages/loginForm/LoginForm.tsx
+++ src/components/pages/loginForm/LoginForm.tsx
@@ -11,7 +11,7 @@ export default function LoginForm() {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        navigate("/home")
+        navigate(`/pokedex/${user.username}`)
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
